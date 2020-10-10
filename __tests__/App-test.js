9b9dd516a6325b4b925beb864866f1b8f8a4942d
Arkadii Shvartcman <yukimurachris@gmail.com>
2020-10-10 15:43:28 +0300

    Initial commit

---
+++ __tests__/App-test.js
@@ -0,0 +1,23 @@
+/**
+ * @format
+ */
+
+import React from 'react';
+import {shallow, configure} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import App from '../App';
+configure({adapter: new Adapter()});
+describe('indirectly testing "incrementCounter" through click simulation', () => {
+  it('should update the count by 1 when invoked by default', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.state('counter')).toBe(0);
+    wrapper.find('button').simulate('click');
+    expect(wrapper.state('counter')).toBe(1);
+  });
+  it('should add two to the count when the "two" value is true', () => {
+    const wrapper = shallow(<App two />);
+    expect(wrapper.state('counter')).toBe(0);
+    wrapper.find('button').simulate('click');
+    expect(wrapper.state('counter')).toBe(2);
+  });
+});
